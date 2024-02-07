beab35a5bfabc0654a3cf70299a2f77040eeed2f
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-02-07 22:40:38 +0200

    further work on a project

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import './main.scss';
+import { mockData } from '../../mockData';
+
+//components
+import Card from '../Card/Card';
 
 function Main() {
+  // console.log(mockData);
   return (
-    <main>
-      <div>
-        <h1>Items</h1>
-      </div>
+    <main className="main-container">
+      {mockData.map((item) => {
+        return (
+          <Card
+            key={item.title}
+            title={item.title}
+            description={item.description}
+          />
+        );
+      })}
     </main>
   );
 }
