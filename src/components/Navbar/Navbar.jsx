fea0167e5d20d602523404b88ac5185cc0345a82
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-02-20 23:24:34 +0200

    fetch from database

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.scss';
 
 function Navbar() {
+  const [user, setUser] = useState('user');
+  let endpoint = 'https://jsonplaceholder.typicode.com/users/1';
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(endpoint);
+
+        if (!response.ok) throw new Error('Something went wrong');
+
+        const data = await response.json();
+
+        console.log(data);
+
+        setUser(data.username);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, [endpoint]);
   return (
     <nav className="nav-container">
       <h1>My Shop</h1>
@@ -17,6 +41,7 @@ function Navbar() {
           <NavLink to="/favorite">Favorite</NavLink>
         </li>
       </ul>
+      {loading ? <h1 className="loading">loading...</h1> : <h1>{user}</h1>}
     </nav>
   );
 }
